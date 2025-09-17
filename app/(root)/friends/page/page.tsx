5897f45ca494f12aef1b4609f049aea686352645
shahzadakram786 <alyakram786@gmail.com>
2025-09-17 13:43:10 +0500

    adding logic to empty braces

--- app/(root)/friends/page/page.tsx
+++ app/(root)/friends/page/page.tsx
@@ -9,7 +9,7 @@ import { api } from '@/convex/_generated/api'
 import { Loader2 } from 'lucide-react'
 import Request from './_components/Request'
 
-type Props = {}
+type Props = Record<string, never>
 
 const FriendsPage = (props: Props) => {
 
